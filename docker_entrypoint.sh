f8f1473146b363116257fa351332712e7795e08e
Patrick <pgovoni21@gmail.com>
2023-03-02 16:37:03 +0100

    deleted excess files

--- docker_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo systemctl unmask influxdb.service && \
-sudo systemctl start influxdb && \
-sudo systemctl enable influxdb.service
-
-EXPERIMENT_NAME=docker_exp python3 -u /app/abm/data/metaprotocol/experiments/docker_exp.py
