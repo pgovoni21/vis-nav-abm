b185015876cd1fbd404220135120afb23be54c8e
pgovoni21 <pgovoni21@gmail.com>
2023-05-08 17:17:22 +0200

    normalized RNN hidden state

--- abm/NN/RNNs.py
+++ abm/NN/RNNs.py
@@ -29,30 +29,28 @@ class RNN(nn.Module):
         # construct architecture of NN
         input_size, hidden_size, output_size = arch
         self.hidden_size = hidden_size
-        
-        self.i2h = nn.Linear(input_size, hidden_size)
-        self.h2h = nn.Linear(hidden_size, hidden_size)
+
+        self.i2h = nn.Linear(input_size, hidden_size, bias=False)
+        self.h2h = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.norm_h = nn.InstanceNorm1d(hidden_size)
         self.h2o = nn.Linear(hidden_size, output_size)
-        self.layers = [self.i2h, self.h2h, self.h2o]
+        self.layers = [self.i2h, self.h2h, self.norm_h, self.h2o]
+        # self.i2h = nn.Linear(input_size, hidden_size)
+        # self.h2h = nn.Linear(hidden_size, hidden_size)
+        # self.h2o = nn.Linear(hidden_size, output_size)
+        # self.layers = [self.i2h, self.h2h, self.h2o]
 
         # disable autograd computation since we're not computing gradient
         for layer in self.layers:
-            layer.requires_grad_(False)
-
-        # construct param vector if not provided by EA
-        if params is None:
-            num_weights = hidden_size * (input_size + hidden_size + output_size)
-            num_biases = 2*hidden_size + output_size
-            params = np.random.randn(num_weights + num_biases) * np.sqrt(2/hidden_size)
-        
-        # assign w+b parameters
-        self.assign_params(params)
+            layer.requires_grad_(False)      
 
         # set activation function
         if activ == 'relu':
             self.activ = torch.relu
         elif activ == 'tanh':
             self.activ = torch.tanh
+        elif activ == 'silu':
+            self.activ = torch.nn.SiLU()
         else:
             raise ValueError(f'Invalid activation function: {activ}')
 
@@ -60,17 +58,35 @@ class RNN(nn.Module):
         # tau = 100
         # self.alpha = dt / tau # default --> alpha = 1
 
+        # # construct param vector if not provided by EA
+        # if params is None:
+        #     # num_weights = hidden_size * (input_size + hidden_size + output_size)
+        #     # num_biases = 2*hidden_size + output_size
+        #     # params = np.random.randn(num_weights + num_biases) * np.sqrt(2/hidden_size)
+        #     num_params = sum(p.numel() for p in self.parameters())
+        #     params = np.random.randn(num_params) * np.sqrt(2/hidden_size)
+
+        # assign w+b parameters
+        if params is not None:
+            self.assign_params(params)
 
     def assign_params(self, param_vector):
 
         # set params by chunking according to model architecture
         param_num = 0
         for l in self.layers:
-            for param in [l.weight, l.bias]:
-
-                chunk = param_vector[param_num : param_num + param.numel()]
-                param.data = chunk.reshape(param.shape)
-                param_num += param.numel()
+            # for param in [l.weight, l.bias]:
+            #     chunk = param_vector[param_num : param_num + param.numel()]
+            #     param.data = chunk.reshape(param.shape)
+            #     param_num += param.numel()
+            if l.weight is not None:
+                chunk = param_vector[param_num : param_num + l.weight.numel()]
+                l.weight.data = chunk.reshape(l.weight.shape)
+                param_num += l.weight.numel()
+            if l.bias is not None:
+                chunk = param_vector[param_num : param_num + l.bias.numel()]
+                l.bias.data = chunk.reshape(l.bias.shape)
+                param_num += l.bias.numel()
 
 
     def forward(self, input, hidden):
@@ -91,10 +107,13 @@ class RNN(nn.Module):
         # converts np.array to torch.tensor + adds 1 dimension --> size [4] becomes [1, 4]
         input = torch.from_numpy(input).float().unsqueeze(0)
 
-        # carry out recurrent calculations according to model formulation (with or without noisy activations)
+        # transform input + normalized hidden state (with or without noisy input)
         # input += torch.randn(input.shape) * self.std_noise_in ## uncomment for noise
         i = self.i2h(input)
-        h = self.h2h(hidden)
+        h = self.norm_h(self.h2h(hidden))
+        # h = self.h2h(hidden)
+
+        # combine + apply nonlinearity (with or without time constant + noisy hidden activity)
         x = self.activ(i + h)
         # x = hidden * (1 - self.alpha) + x * self.alpha ## uncomment for time constant
         # x += torch.randn(hidden.shape) * self.std_noise_rec ## uncomment for noise
