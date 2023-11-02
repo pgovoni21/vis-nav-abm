9165bfb54e88d6d9af2ac9bcf46b4b3c4fbbe7a7
pgovoni21 <pgovoni21@gmail.com>
2023-11-02 13:36:18 +0100

    added val NNcen

--- abm/monitoring/validation.py
+++ abm/monitoring/validation.py
@@ -6,7 +6,7 @@ import numpy as np
 import multiprocessing as mp
 
 
-def rerun_topNNs(name, num_NNs=5, num_seeds=3):
+def rerun_topNNs(name, num_NNs=5, num_seeds=3, best=True):
 
     print(f'running: {name}')
 
@@ -32,7 +32,9 @@ def rerun_topNNs(name, num_NNs=5, num_seeds=3):
     mp_inputs = []
     for g,f in zip(top_ind, top_fit):
 
-        NN_pv_path = fr'{data_dir}/{name}/gen{g}_NN0_pickle.bin'
+        if best: NN_pv_path = fr'{data_dir}/{name}/gen{g}_NN0_pickle.bin'
+        else:    NN_pv_path = fr'{data_dir}/{name}/gen{g}_NNcen_pickle.bin'
+
         with open(NN_pv_path,'rb') as f:
             pv = pickle.load(f)
 
@@ -44,44 +46,46 @@ def rerun_topNNs(name, num_NNs=5, num_seeds=3):
         results = pool.starmap_async( start, mp_inputs)
         pool.close()
         pool.join()
-
-    # unpack results into matrix + save
     results_list = results.get()
 
     # skip to start of each seed series/chunk + allocate fitness to save matrix
     for i,c in enumerate(range(0, len(results_list), num_seeds)):
         for s,(_, fitnesses, _) in enumerate(results_list[c : c + num_seeds]):
-
             val_matrix[i,s] = round(fitnesses[0],0)
-
-    # dump raw data
-    with open(fr'{exp_path}/val_matrix.bin', 'wb') as f:
-        pickle.dump(val_matrix, f)
 
     # take avg + print results
     avg_per_NN = np.average(val_matrix, axis=1).round(1)
     for i, ef, vf in zip(top_ind, top_fit, avg_per_NN):
         print(f'gen: {i} | EA_fit: {int(ef)} | val_fit: {int(vf)}')
 
-    # writing txt file
-    with open(fr'{exp_path}/val_results.txt', 'w') as f:
-        f.write(f'Validation matrix shape (num_NNs, num_seeds): {val_matrix.shape}\n')
-        for i, ef, vf in zip(top_ind, top_fit, avg_per_NN):
-            f.write(str(f'gen: {i} | EA_fit: {int(ef)} | val_fit: {int(vf)}\n'))
+    # dump raw data + txt file
+    if best:
+        with open(fr'{exp_path}/val_matrix.bin', 'wb') as f:
+            pickle.dump(val_matrix, f)
+        with open(fr'{exp_path}/val_results.txt', 'w') as f:
+            f.write(f'Validation matrix shape (num_NNs, num_seeds): {val_matrix.shape}\n')
+            for i, ef, vf in zip(top_ind, top_fit, avg_per_NN):
+                f.write(str(f'gen: {i} | EA_fit: {int(ef)} | val_fit: {int(vf)}\n'))
+    else:
+        with open(fr'{exp_path}/val_matrix_cen.bin', 'wb') as f:
+            pickle.dump(val_matrix, f)
+        with open(fr'{exp_path}/val_results_cen.txt', 'w') as f:
+            f.write(f'Validation matrix shape (num_NNs, num_seeds): {val_matrix.shape}\n')
+            for i, ef, vf in zip(top_ind, top_fit, avg_per_NN):
+                f.write(str(f'gen: {i} | EA_fit: {int(ef)} | val_fit: {int(vf)}\n'))
 
 
 if __name__ == '__main__':
 
     names = []
 
-    # names = [f'singlecorner_exp_CNN1124_FNN2_p50e15_vis8_rep{x}' for x in range(5)]
+    # names = [f'singlecorner_exp_CNN1124_FNN2_p200e20_vis8_rep{x}' for x in range(3)]
     # for name in names:
     #     rerun_topNNs(name, num_NNs=20, num_seeds=100)
     
-    # names = [f'singlecorner_exp_CNN1124_FNN2_p50e20_vis8_rep{x}' for x in range(3)]
+    # names = [f'singlecorner_exp_CNN1124_FNN2_p50e20_vis8_PGPE_rep{x}' for x in range(2)]
     # for name in names:
     #     rerun_topNNs(name, num_NNs=20, num_seeds=100)
 
-    names = [f'singlecorner_exp_CNN1124_FNN2_p50e20_vis8_rep3']
-    for name in names:
-        rerun_topNNs(name, num_NNs=20, num_seeds=100)
+    # rerun_topNNs('singlecorner_exp_CNN1124_FNN2_p50e20_vis8_PGPE_ss10_rep0', num_NNs=20, num_seeds=100)
+    rerun_topNNs('singlecorner_exp_CNN1124_FNN2_p50e20_vis8_PGPE_ss15_rep4', num_NNs=20, num_seeds=100, best=False)
